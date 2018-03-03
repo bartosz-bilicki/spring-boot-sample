44a750e1c05f96c397757266dd1a6dcd176f3ee8
bb.bartosz <bb.bartosz@gmail.com>
2018-03-03 13:00:49 +0100

    Revert "directory rename"
    This reverts commit 96328a1c57d69e6722092a8a5e2441315a9e85c5.

---
+++ complete/src/main/java/hello/Application.java
@@ -0,0 +1,36 @@
+package hello;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    //optional, could we used for batch jobs
+    @Bean
+    public CommandLineRunner commandLineRunner(ConfigurableApplicationContext ctx) {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                System.out.println("Let's inspect the beans provided by Spring Boot:");
+                String[] beanNames = ctx.getBeanDefinitionNames();
+                Arrays.sort(beanNames);
+                for (String beanName : beanNames) {
+                    System.out.println(beanName);
+                }
+                // if you want to exit application after the job
+                //ctx.close();
+            }
+        };
+    }
+
+}
---
+++ complete/src/main/java/hello/PersonDatabase.java
@@ -0,0 +1,31 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+import java.lang.invoke.MethodHandles;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class PersonDatabase {
+    private static final Logger LOG= LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private final List<Person> personList;
+
+    public PersonDatabase() {
+        this(Arrays.asList(
+                Person.builder().name("person1").build(),
+                Person.builder().name("person2").build()
+        ));
+    }
+
+    public PersonDatabase(List<Person> personList) {
+        this.personList=personList;
+        LOG.info("I am your database");
+    }
+
+    public List<Person> findAll() {
+        return personList;
+    }
+}
